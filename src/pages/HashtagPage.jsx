2d9b06bb7e8d988901d1ba2f254dd507ce56a720
Estevam <estevamfurtado@gmail.com>
2022-06-15 12:25:35 -0300

    Feat: hashtag page +  Refact: posts

--- src/pages/HashtagPage.jsx
+++ src/pages/HashtagPage.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Axios from '../adapters';
+import Feed from '../components/Feed';
+
 export default function HashtagPage() {
-  return (
-    <div>
-      <h1>HashtagPage</h1>
-    </div>
-  );
+  const { hashtag } = useParams();
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const promise = Axios.get(`/hashtags/${hashtag}/posts`).then((res) => {
+      console.log(res.data);
+      setPosts(res.data);
+    });
+  }, []);
+
+  return <Feed title={`# ${hashtag}`} posts={posts} canCreatePost={true} />;
 }
