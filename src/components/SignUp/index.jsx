404403c3be6045a8c2075851056f18c15d295a02
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-06-16 08:53:48 -0300

    feat: new components structure

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -4,12 +4,14 @@ import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 import Axios from '../../adapters/index.jsx';
 
-import getRandomInt from './../../utils/getRandomInt.js';
+import Axios from '../../adapters';
+import DataContext from '../../contexts/DataContext';
+import getRandomInt from '../../utils/getRandomInt.js';
 
-import StyledLoadingDots from './../../layout/StyledLoadingDots';
-import StyledInput from './../../layout/StyledInput';
-import StyledLink from './../../layout/StyledLink';
-import StyledButton from './../../layout/StyledButton';
+import StyledLoadingDots from '../../styles/StyledLoadingDots.jsx';
+import StyledInput from '../../styles/StyledInput.jsx';
+import StyledLink from '../../styles/StyledLink.jsx';
+import StyledButton from '../../styles/StyledButton.jsx';
 import StyledPage from './styles';
 
 function SignUp() {
@@ -94,7 +96,7 @@ function SignUp() {
 
     async function handleSignup() {
       try {
-        const URL = `auth/sign-up`;
+        const URL = '/auth/sign-up';
         const body = {
           username: formData.username,
           password: formData.password,
@@ -103,7 +105,7 @@ function SignUp() {
         };
 
         const response = await Axios.post(URL, body);
-        response.status === 200 || 201 ? handleSuccess(response) : handleError();
+        response.status === 200 ? handleSuccess(response) : handleError();
       } catch (error) {
         handleError(error);
         resetAll();
