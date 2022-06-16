00d2c117a8f39d735e3e4088f72981b12cd3093e
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-06-16 08:50:35 -0300

    feat: added unique endpoint for accessing database

--- src/adapters/index.jsx
+++ src/adapters/index.jsx
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL||'http://localhost:4000',
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000',
 });
 
 export default Axios;
