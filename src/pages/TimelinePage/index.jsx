db1c5d1f881a45830f5606cf64786d312208a63c
Nivaldo Farias <badivia.f@gmail.com>
2022-06-17 11:07:40 -0300

    fix: TimelinePage file conflicts

--- src/pages/TimelinePage/index.jsx
+++ src/pages/TimelinePage/index.jsx
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
-import TimelinePosts from '../../components/TimelinePosts';
-=======
 import { useEffect, useState } from 'react';
 import Axios from '../../adapters';
-import Feed from "../../components/Feed";
->>>>>>> main
+import Feed from '../../components/Feed';
 
 export default function TimelinePage() {
-
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-      const promise = Axios.get(`/timeline`);
-      promise.then(({ data }) => {setPosts(data)})}
-      , []);
+    const promise = Axios.get(`/timeline`);
+    promise.then(({ data }) => {
+      setPosts(data);
+    });
+  }, []);
 
-  return (
-    <Feed title={`timeline`} posts={posts} canCreatePost={true} userThumbnail={false}/>
-  );
+  return <Feed title={`timeline`} posts={posts} canCreatePost={true} userThumbnail={false} />;
 }
