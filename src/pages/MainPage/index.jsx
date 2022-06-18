7a82ceb7db1510ce1c371aef93e4f36dc50b39d7
Estevam <estevamfurtado@gmail.com>
2022-06-18 14:43:23 -0300

    Feat: load user

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -6,23 +6,26 @@ import DataContext from './../../hooks/DataContext';
 import { Wrapper, Main, Feed } from './styles';
 import { useNavigate } from 'react-router-dom';
 import MouseContext from '../../hooks/MouseContext';
+import Axios from '../../blueprints';
 
 export default function MainPage() {
-  const { width, token } = useContext(DataContext);
+  const { width, token, setUser } = useContext(DataContext);
   const { setClicking } = useContext(MouseContext);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!token) {
       navigate('/');
+    } else {
+      const config = { headers: { Authorization: `Bearer ${token}` } };
+      Axios.get('/users', config).then((res) => {
+        setUser(res.data.user);
+      });
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token]);
 
   return (
-    <Wrapper 
-      onMouseDown={() => setClicking(true)}
-      onMouseUp={() => setClicking(false)}>
+    <Wrapper onMouseDown={() => setClicking(true)} onMouseUp={() => setClicking(false)}>
       <Header />
       <Main>
         <Feed>
