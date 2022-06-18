0b3261a73dae03512fff9a85f623796fe79557e9
santannaizadora <santannaizadora@gmail.com>
2022-06-18 16:09:25 -0300

    feat: loading component

---
+++ src/components/Loading/styles/index.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const LoadingContainer = styled.div`
+    ${({ theme }) => theme.mixins.flexbox('row', 'center', 'center', '18px')};
+    padding: 18px;
+    width: 100%;
+
+    border-radius: 16px;
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.post};
+`;
+
+export default LoadingContainer;
--- src/pages/TimelinePage/index.jsx
+++ src/pages/TimelinePage/index.jsx
@@ -1,11 +1,13 @@
 import { useContext, useEffect, useState } from 'react';
 import Axios from '../../blueprints';
 import Feed from '../../components/Feed';
+import Loading from '../../components/Loading';
 import DataContext from '../../hooks/DataContext';
 import toast, { Toaster } from 'react-hot-toast';
 
 export default function TimelinePage() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { token } = useContext(DataContext);
 
   useEffect(() => {
@@ -22,6 +24,7 @@ export default function TimelinePage() {
     const promise = Axios.get(`/timeline`, config);
     promise.then(({ data }) => {
       setPosts(data);
+      setLoading(false);
     });
     promise.catch(function(error){
       toast.error('An error occured while trying to fetch the posts, please refresh the page')
@@ -32,17 +35,18 @@ export default function TimelinePage() {
   }
   return (
     <>
-    <Toaster
-      position="top-right"
-      reverseOrder={false}
-    />
-    <Feed
+    <Toaster position="top-right" reverseOrder={false}/>
+    {loading 
+      ?
+      <Loading />
+      :
+      <Feed
       title={`timeline`}
       posts={posts}
       canCreatePost={true}
       userThumbnail={false}
       updatePostsFunction={updateTimeline}
-    />
+    />}
     </>
   );
 }
